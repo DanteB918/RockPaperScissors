c46a8374dc41aa1524dd0cc63f73a33f3388e2c3
DanteB918 <100642899+DanteB918@users.noreply.github.com>
2022-03-13 14:53:17 -0400

    Add files via upload

---
+++ Rock, Paper, Scissors!.py
@@ -0,0 +1,44 @@
+import random
+ans = random.randint(1, 3)
+if ans == 1:
+    print('Rock!')
+if ans == 2:
+    print('Paper!')
+if ans == 3:
+    print('Scissors')
+print('Welcome to Rock Paper Scissors!')
+def rock():
+    if input() == rock():
+        print(ans)
+        if ans == 1:
+            pritn('We were tied!')
+        if ans == 2:
+            print('you were defeated!')
+        if ans == 3:
+            print('you won!!')
+input('Which would you like to choose? \nRock, Paper or scissors?:')
+if input() == 'rock':
+    if ans == 1:
+        print('Rock \nWe were tied!')
+    if ans == 2:
+        print('Paper \nyou were defeated!')
+    if ans == 3:
+        print('Scissors \nyou won!!')
+if input() == 'paper':
+    if ans == 1:
+        print('Rock \nyou won!!')
+    if ans == 2:
+        print('Paper \nWe were tied!')
+    if ans == 3:
+        print('Scissors \nyou were defeated!')
+if input() == 'scissors':
+    if ans == 1:
+        print('Rock \nyou were defeated!')
+    if ans == 2:
+        print('Paper \nyou won!!')
+    if ans == 3:
+        print('Scissors \nWe were tied!')
+else:
+    print('Thats not an option..')
+    quit()
+    
