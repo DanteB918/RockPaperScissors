1b985d71ebe93d3f6ad075b15a780e170d740a2a
DanteB918 <100642899+DanteB918@users.noreply.github.com>
2022-04-21 11:23:52 -0400

    Update Rock, Paper, Scissors!.py

--- Rock, Paper, Scissors!.py
+++ Rock, Paper, Scissors!.py
@@ -1,6 +1,6 @@
 import random
 
-ans = random.randint(1, 3)
+ans = random.randint(1, 3)          #Picks a random interger, 1, 2 or 3, and assigns them to rock, paper and scissors.
 if ans == 1:
     print('Rock!')
 if ans == 2:
@@ -10,7 +10,7 @@ if ans == 3:
 print('Welcome to Rock Paper Scissors!')
 
 n = input('Which would you like to choose? \nRock, Paper or scissors?:')
-if n == 'rock'.casefold():
+if n == 'rock'.casefold():                      #Assigning responses to answers, if player chooses rock and the computer chooses paper, the player loses and vice versa.
     if ans == 1:
         print('Rock \nWe were tied!')
     if ans == 2:
